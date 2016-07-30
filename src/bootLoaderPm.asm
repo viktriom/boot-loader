8f700929d2a65d6c6efd5ec147af98f6a429f4a3
Vivek Tripathi <viktriom@gmail.com>
2016-07-30 22:05:57 +0530

    updated boot loader and added disk load utility

--- src/bootLoaderPm.asm
+++ src/bootLoaderPm.asm
@@ -3,12 +3,15 @@
 
 [org 0x7c00]
 
+KERNEL_OFFSET equ 0x1000    ;This is the memory offset to which we will load our kernel.
+
 mov bp, 0x9000		;setting up the stack
 mov sp, bp
 
 mov si, MSG_REAL_MODE
-
 call print_string_real
+
+call load_kernel        ;Call routine to laod the kernel.
 
 call switch_to_pm		;Note that we never return from here.
 
@@ -18,21 +21,39 @@ call switch_to_pm		;Note that we never return from here.
 %include "gdt.asm"
 %include "printStringPm.asm"
 %include "protectedMode.asm"
+%include "diskLoad.asm"
 
 ;This is where we arrive after switching to and initializing protected mode.
 
+[bits 16]
+
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL     ;Display loading kernel message
+    call print_string_real
+
+    mov bx, KERNEL_OFFSET
+    mov dh, 15
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+
+    ret
+
+[bits 32]
+
 BEGIN_PM:
 
-mov ebx, MSG_PROT_MODE
-call print_string_pm		;user our 32-bit print routine
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm		;user our 32-bit print routine
 
-;jmp $
+    ;jmp $
 
 ;Global Variables
 
+BOOT_DRIVE db 0
 MSG_REAL_MODE: db "Started in 16 bit Real Mode",0
 MSG_PROT_MODE: db "Successfully landed in 32-bit protected mode",0
 MSG_TEMP: db "BEGIN PM Called",0
+MSG_LOAD_KERNEL db "Loading kernel into memory.",0
 
 ;Finaly the boot loader padding (the magic number)
 times 510-($-$$) db 0
